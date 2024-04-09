a8f63de8758d87efe70c4c940d49cbf912ad2a28
PavelMart <martackovp@yandex.ru>
2024-04-09 20:58:06 +0300

    Implemented Spring-context with ApplicationContext

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,16 +1,15 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
 import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
 import ru.netology.router.Router;
 import ru.netology.service.PostService;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
@@ -18,10 +17,12 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        final var service = context.getBean(PostService.class);
+        final var repository = context.getBean(PostRepository.class);
         controller = new PostController(service);
         router = new Router(controller);
+
     }
 
     @Override
